f8e6afaf229d5705d74517e986bd545abd65c0e3
MCKENT05 <atemitope95@gmail.com>
2022-02-07 16:09:56 +0100

    added methods visible in the UML

--- items.rb
+++ items.rb
@@ -1,12 +1,11 @@
 class Item
-  attr_reader :genre, :author, :source, :label
+  attr_reader :genre, :author, :label
   attr_accessor :publish_date
 
   def initialize(publish_date, archived: false)
     @id = Random.rand(1...1000)
     @genre = genre
     @author = author
-    @source = source
     @label = label
     @publish_date = publish_date
     @archived = archived
@@ -16,4 +15,23 @@ class Item
     @label = label
     @label.items.push(self) unless @label.items.include?(self)
   end
+
+  def author=(author)
+    @author = author
+    @author.items.push(self) unless @author.items.include?(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    @genre.items.push(self) unless @genre.items.include?(self)
+  end
+
+  def can_be_archived?
+   true
+  end
+
+  def move_to_archive?
+    true if can_be_archived?
+  end
+
 end
