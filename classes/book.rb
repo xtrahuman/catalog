4afdc4382f007577c857c03200af6ac599ce661d
MCKENT05 <atemitope95@gmail.com>
2022-02-09 19:40:07 +0100

    added save functionality

--- classes/book.rb
+++ classes/book.rb
@@ -1,7 +1,7 @@
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize( publisher, cover_state, publish_date:, archived: false)
+  def initialize(publisher, cover_state, publish_date:, archived: false)
     super(publish_date: publish_date, archived: archived)
     @publisher = publisher
     @cover_state = cover_state
