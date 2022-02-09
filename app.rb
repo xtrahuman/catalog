8747b647c25667455dc61710dd1b8fac692919ea
Okino <37089367+yaseerokino@users.noreply.github.com>
2022-02-09 13:07:06 +0100

    Update methods for menu options

--- app.rb
+++ app.rb
@@ -1,39 +1,42 @@
-require_relative 'modules/get_game'
-require_relative './modules/albumfunction'
+require_relative './functions/game_function'
+require_relative './functions/album_function'
+require_relative './functions/book_function'
 
 class App
   def initialize
     @games = []
-    @get_game = AllGame.new(@games)
+    @game_list = GameFunction.new(@games)
     @albums = []
     @album_list = AlbumFunction.new(@albums)
+    @books = []
+    @book_list = BookFuction.new(@books)
   end
 
   def get_num(option)
     case option
     when '1'
-      puts 'list_all_books'
+      @book_list.list_all_books
     when '2'
       @album_list.list_albums
     when '3'
-      puts @get_game.list_all_games
+      @game_list.list_all_games
     when '4'
       @album_list.list_genres
     when '5'
-      puts 'list_all_labels'
+      @book_list.list_all_labels
     when '6'
-      puts @get_game.list_all_authors
+      puts @game_list.list_all_authors
     end
   end
 
   def add_num(option)
     case option
     when '7'
-      puts 'Add a book'
+      @book_list.add_book
     when '8'
       @album_list.add_music_album
     when '9'
-      puts @get_game.add_game
+      @game_list.add_game
     end
   end
 end
